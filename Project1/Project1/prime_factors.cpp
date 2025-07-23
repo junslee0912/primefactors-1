1635dffd665c4dd9972e6ea7b46ac32957f371a0
Junsecuk <junslee0912@gmail.com>
2025-07-23 10:21:57 +0900

    [Refactor] P66

--- Project1/Project1/prime_factors.cpp
+++ Project1/Project1/prime_factors.cpp
@@ -3,27 +3,16 @@ using std::vector;
 class PrimeFactors {
 public:
 	vector<int> of(int number)
-	{	
+	{
 		vector<int> result = {};
-		if (number > 1) {
-			int divisor = 2;
-			if (number == 4 || number == 6 || number == 9 || number == 12)
+		for (int divisor = 2; number > 1; divisor++)
+		{
+			while (number % divisor == 0)
 			{
-				for (divisor = 2; number > 1; divisor++)
-				{
-					while (number % divisor == 0)
-					{
-						result.push_back(divisor);
-						number /= divisor;
-					}
-				}
-			}
-
-			else {
-				result.push_back(number);
+				result.push_back(divisor);
+				number /= divisor;
 			}
 		}
-
 		return result;
 	}
 };
